4049dd9e04afca5e1041796ba5c947b0527cddb7
mattn <mattn.jp@gmail.com>
2013-01-06 23:25:12 +0900

    show description in example.rb

--- example/example.rb
+++ example/example.rb
@@ -1,16 +1,20 @@
 #!mruby
 
 v8 = V8.new
-puts v8.eval('1+2')
+expr = '1 + 2'
+puts "#{expr} = #{v8.eval(expr)}"
 
 v8.add_func("plus") do |lhs,rhs|
   lhs + rhs
 end
 
-puts v8.eval("plus(2,3)")
+expr = 'plus(2, 3)'
+puts "#{expr} = #{v8.eval(expr)}"
 
 v8.eval("var console = {}");
 v8.add_func("console.log") do |str|
   puts str
 end
-v8.eval("console.log(123)")
+expr = 'console.log(123)'
+puts expr
+v8.eval(expr)
